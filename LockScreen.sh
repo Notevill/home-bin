f567e3dce02feaff1ea40e6c6b5128b3ae220b1e
Notevill <Notevill@gmail.com>
2020-02-27 15:28:49 +0500

    replace scrot by maim

--- LockScreen.sh
+++ LockScreen.sh
@@ -1,7 +1,7 @@
-# /bin/bash
+#!/bin/bash
 
 #   Простая блокировка экрана
-scrot $HOME/Pictures/rscreen.png
+maim $HOME/Pictures/rscreen.png
 convert $HOME/Pictures/rscreen.png -blur 0x3 ~/Pictures/bscreen.png
 i3lock -i $HOME/Pictures/bscreen.png
 rm $HOME/Pictures/*screen.png
